a6ab7809e1b079cbc3188a8e0e0fa2d7dd411d1f
Seidali_ <117403561+Seidali001@users.noreply.github.com>
2023-06-15 18:30:00 +0100

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,38 +1,57 @@
 import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        tags: {
+            type: Array,
+            default: []
+        },
+        viewsCount: {
+            type: Number,
+            default: 0
+        },
+        comments: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Comment",
+                    required: false
+                },
+                avatarUrl: {
+                    type: String,
+                    ref: "Comment",
+                    required: false
+                },
+                fullName: {
+                    type: String,
+                    ref: "Comment",
+                    required: false
+                },
+                content: {
+                    type: String,
+                    ref: "Comment",
+                    required: false
+                }
+            }
+        ],
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        imageUrl: String,
     },
-    text: {
-        type: String,
-        required: true,
-        unique: true
+    {
+        timestamps: true,
     },
-    /* tags: {
-        type: Array,
-        default: ["react", "redax", "node"]
-    },  */
-    tags: {
-        type: mongoose.Schema.Types.Array, // Используем встроенную схему для массива тегов
-        ref: "Tag",
-        required: true
-      },
-    viewsCount: {
-        type: Number,
-        default: 0
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    imageUrl: String,
-}, 
-{
-    timestamps: true,
-},
 );
 
 export default mongoose.model("Post", PostSchema)
