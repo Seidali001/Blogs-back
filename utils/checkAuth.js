88575cd6035a2e23e2d0df6e839ac025da6724f3
Seidali001 <seitmambetovseidali@gmail.com>
2023-06-18 23:12:37 +0100

    will be clean

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import jwt  from "jsonwebtoken";
 
 export default (req, res, next) => {
@@ -24,40 +23,4 @@ next();
         })
     }
 
-    
-    
-
-
 }
-=======
-import jwt  from "jsonwebtoken";
-
-export default (req, res, next) => {
-    const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
-
-    if (token) {
-        try {
-const decoded = jwt.verify(token, "confid123")
-req.userId = decoded._id;
-next();
-        }
-        catch (e) {
-            return res.status(403).json({
-                /* message: "Нет доступа" */
-                message: "No access"
-            })
-        }
-
-    } else {
-       return res.status(403).json({
-            /* message: "Нет доступа" */
-            message: "No access"
-        })
-    }
-
-    
-    
-
-
-}
->>>>>>> 4566e7ad33f9df687c5bf31d689ec6252e3d8d3a
