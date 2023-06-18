36d0495cf109b0e0c7c8b454f186347a757e27f9
Seidali_ <117403561+Seidali001@users.noreply.github.com>
2023-06-18 19:36:22 +0100

    Update checkAuth.js

---
+++ utils/checkAuth.js
@@ -0,0 +1,30 @@
+import jwt  from "jsonwebtoken";
+
+export default (req, res, next) => {
+    const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+
+    if (token) {
+        try {
+const decoded = jwt.verify(token, "confid123")
+req.userId = decoded._id;
+next();
+        }
+        catch (e) {
+            return res.status(403).json({
+                /* message: "Нет доступа" */
+                message: "No access"
+            })
+        }
+
+    } else {
+       return res.status(403).json({
+            /* message: "Нет доступа" */
+            message: "No access"
+        })
+    }
+
+    
+    
+
+
+}
